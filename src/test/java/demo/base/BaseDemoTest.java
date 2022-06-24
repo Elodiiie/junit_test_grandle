629941b8ac4210a0e9a47c99e70b8f37a6c0e85b
Elodiiie <glx983793727@163.com>
2022-06-24 20:58:01 +0800

    add two test cases for method sum

--- src/test/java/demo/base/BaseDemoTest.java
+++ src/test/java/demo/base/BaseDemoTest.java
@@ -14,4 +14,21 @@ public class BaseDemoTest {
 
         assertEquals(expected, baseDemo.print());
     }
+
+    @Test
+    public void should_return_sum_2_when_arguments_equals(){
+        BaseDemo baseDemo = new BaseDemo();
+        int first_argument = 1;
+        int second_argument = first_argument;
+        int expected = 2;
+        assertEquals(expected,baseDemo.sum(first_argument,second_argument));
+    }
+    @Test
+    public void should_return_sum_3_when_arguments_not_equal(){
+        BaseDemo baseDemo = new BaseDemo();
+        int first_argument = 1;
+        int second_argument = 2;
+        int expected = 3;
+        assertEquals(expected,baseDemo.sum(first_argument,second_argument));
+    }
 }
